82cff8ea4b2ca7467ab6253c700e25845d7afb34
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-07 16:45:19 -0300

    componente toast

--- app/src/main/java/com/luizeduardobrandao/componentesinterface/MainActivity.kt
+++ app/src/main/java/com/luizeduardobrandao/componentesinterface/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.luizeduardobrandao.componentesinterface
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,11 +26,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Componente Toast
+        binding.buttonToast.setOnClickListener(this)
     }
 
     override fun onClick(v: View){
         when (v.id) {
 
+            // Toast
+            R.id.button_toast -> {
+                val toast = Toast.makeText(this, "Toast", Toast.LENGTH_SHORT)
+                // API 30+ ignora Toast.setGravity() em toasts padrão.
+
+                // Exibindo a toast
+                toast.show()
+            }
         }
     }
 }
